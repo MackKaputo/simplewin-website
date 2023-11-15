f08aef2e7e7900436b2d86197c0b329baeb19ac0
Mack Kaputo <mackkaputo@gmail.com>
2023-11-16 00:00:16 +0200

    right image at hero

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -8,7 +8,7 @@ import {
 import {TbBrandCashapp} from "react-icons/tb"
 import { GiTrophy } from "react-icons/gi"
 import Image from 'next/image'
-import gameTableImage from "../public/images/game_table_view.png"
+import gameTableImage from "../public/images/game_table_view.jpg"
 
 export default function HeroSection() {
     return(
